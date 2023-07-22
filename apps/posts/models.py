31d0f9c88b0765bea7e85d2cd1d8df29a29777e6
itek <itybek@gmail.com>
2023-07-22 14:00:03 +0600

    add class post

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+#! 1 models.py - Классы 
+#! 2 admin.py - Регистрируем класс
+#! 3 views.py - Пишем функцию
+# 4 urls.py - Указываем путь где будет работать функция
+# 5 templ.html - Выводили данные класса
+
+class Post(models.Model):
+    image = models.ImageField(
+        upload_to='post_images/'
+    )
+    description = models.TextField()
+    created = models.DateTimeField(
+        auto_now_add=True
+    )
+    
+    def __str__(self):
+        return self.description[:15]
+    
