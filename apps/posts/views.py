4647f1bf4485e9cc0a3056ade6e1fe85d33f9639
itek <itybek@gmail.com>
2023-07-29 14:53:40 +0600

    add CRUDfor posts

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -14,12 +14,12 @@ def create_post(request):
     if request.method == 'POST':
         post_image = request.FILES.get('post_image')
         description = request.POST.get('description')
-        # try:
-        post = Post.objects.create(image=post_image, description=description, owner=request.user)
-        post.save()
-        return redirect('index')
-        # except:
-        #     return HttpResponse("Не получилось создать пост")
+        try:
+            post = Post.objects.create(image=post_image, description=description, owner=request.user)
+            post.save()
+            return redirect('index')
+        except:
+            return HttpResponse("Не получилось создать пост")
     return render(request, 'create_post.html')
 
 def post_detail(request,id):
@@ -28,3 +28,28 @@ def post_detail(request,id):
         'post':post
     }
     return render(request, 'comment.html', context)
+
+def post_update(request, id):
+    post = Post.objects.get(id = id)
+    if request.user != post.owner:
+        return redirect('index')
+    if request.method =="POST":
+        image = request.FILES.get('image')
+        description = request.POST.get('description')
+        if image:
+            post.image = image
+            post.description = description
+        else:
+            post.description = description
+        post.save()
+        return redirect('post_detail',post.id)
+    return render(request, 'post_update.html', context={'post':post})
+
+def delete_post(request, id):
+    post = Post.objects.get(id = id)
+    if request.user != post.owner:
+        return redirect('index')
+    if request.method == 'POST':
+        post.delete()
+        return redirect('index')
+    return render(request, 'post_delete.html', context={'post':post})
